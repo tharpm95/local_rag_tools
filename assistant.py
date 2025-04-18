f48e3d465f4d70f820dac01b29a27a953ccfdb18
Matt Tharp <tharpmatt95@gmail.com>
2025-04-18 16:44:03 -0700

    Add chromadb

--- assistant.py
+++ assistant.py
@@ -1,5 +1,24 @@
 import ollama
+import chromadb
 
+client = chromadb.Client()
+message_history = [
+    {
+        'id': '1',
+        'prompt': 'Hello, how are you?',
+        'response': 'I am doing well, thank you!'
+    },
+    {
+        'id': '2',
+        'prompt': 'Hello, how aree you?',
+        'response': 'I am doing weell, thank you!'
+    },
+    {
+        'id': '3',
+        'prompt': 'Hello, how areee you?',
+        'response': 'I am doing weeell, thank you!'
+    }
+]
 convo = []
 
 def stream_response(prompt):
@@ -16,6 +35,44 @@ def stream_response(prompt):
     print('\n')
     convo.append({'role': 'assistant', 'content': response})
 
+def create_vector_db(conversations):
+    vector_db_name = 'conversations'
+
+    try:
+        client.delete_collection(name=vector_db_name)
+    except ValueError:
+        pass
+
+    vector_db = client.create_collection(name=vector_db_name)
+
+    for c in conversations:
+        serialized_convo = f'prompt: {c["prompt"]} response: {c["response"]}'
+        response = ollama.embeddings(model='nomic-embed-text', prompt=serialized_convo)
+        embedding = response['embedding']
+
+        vector_db.add(
+            ids=[str(c['id'])],
+            embeddings=[embedding],
+            documents=[serialized_convo]
+        )
+
+def retreive_embeddings(prompt):
+    response = ollama.embeddings(model='nomic-embed-text', prompt=prompt)
+    prompt_embedding = response['embedding']
+
+    vector_db = client.get_collection(name='conversations')
+    results = vector_db.query(
+        query_embeddings=[prompt_embedding],
+        n_results=1,
+    )
+    best_embedding = results['documents'][0][0]
+
+    return best_embedding
+
+create_vector_db(conversations=message_history)
+
 while True:
     prompt = input('USER: \n')
-    stream_response(prompt)
+    context = retreive_embeddings(prompt=prompt)
+    prompt = f'USER PROMPT: {prompt} CONTEXT: {context}'
+    stream_response(prompt=prompt)
